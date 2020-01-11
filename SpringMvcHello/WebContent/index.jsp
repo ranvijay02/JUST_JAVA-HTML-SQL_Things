0bd90a7dc41e110c21a7f305859d94e0ae4f9231
RANVIJAY TANWAR <42860273+ranvijay02@users.noreply.github.com>
2020-01-11 16:09:32 +0530

    Add files via upload
    Spring Model-View-Controller Pattern

---
+++ SpringMvcHello/WebContent/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>spring mvc</title>
+</head>
+<body>
+<h1>Welcome to Spring MVC</h1>
+
+<a href="home">Click kro idhar</a>
+<a href= "/service/greeting/RV">Greetings</a> 
+</body>
+</html>
---
+++ SpringMvcHello/src/com/deloitte/MyRestController.java
@@ -0,0 +1,17 @@
+package com.deloitte;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service/greeting")
+public class MyRestController {
+	@RequestMapping (value = "/{name}" , method=RequestMethod.GET)
+	public String getGreeting( @PathVariable String name) {
+    String result = " Hi "+name+", Greetings from REST Comntroller";
+		return "";
+	}
+
+}
